4b2d8193b3444b016fa9b726139b4c1c23bdb57a
rahmat <cememeo@rahmat.local>
2023-04-12 16:17:24 +0700

    init

---
+++ 2_get_request.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<ul>
+    <li><a href="2_get_response.php?sign=go" style="color:green">Go</a></li>
+    <li><a href="2_get_response.php?sign=slow" style="color:orange">Slow</a></li>
+    <li><a href="2_get_response.php?sign=stop" style="color:red">Stop</a></li>
+</ul>
+
+</body>
+</html>
---
+++ 2_get_response.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$sign = $_GET['sign'];
+?>
+
+<p>Sign : <?php echo $sign;?></p>
+
+</body>
+</html>
